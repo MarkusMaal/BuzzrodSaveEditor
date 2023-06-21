439674e37a95673c993a141ce1b7664f788fb6e0
Markus Maal <markus.maal@gmail.com>
2023-06-21 22:13:12 +0300

    Revert "Added list saves function"
    This reverts commit d3d65eb1103d0825baf9e507562df85427d5af1c.

--- make_corrupted_file.py
+++
@@ -1,6 +0,0 @@
-from random import randint
-output = "RANDOM"
-with open(output, 'wb') as out:
-    for i in range(10408):
-        n = randint(1, 5)
-        out.write(int(n).to_bytes(1, "little"))
