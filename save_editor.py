dcc84a579b9b7d32ef76ada6652ac082c3be08ee
Markus Maal <markus.maal@gmail.com>
2023-06-21 22:13:49 +0300

    Added list saves function

--- save_editor.py
+++ save_editor.py
@@ -3,6 +3,13 @@ import sys, os
 slot_table = "Best Push,Sink Fast,Once Uncut,Twice Uncut,Once Ok,Twice Ok,Gambler,Anti-Arrows,Sleeping Pill,Guts,Outstanding,Float Reverse,Vibration Up 1,Vibration Down 1,Smell Up 1,Smell Down 1,Light Up 1,Light Down 1,Sound Up 1,Sound Down 1,Vibration Up 2,Vibration Down 2,Smell Up 2,Smell Down 2,Light Up 2,Light Down 2,Sound Up 2,Sound Down 2,Vibration Up 3,Vibration Down 3,Smell Up 3,Smell Down 3,Light Up 3,Light Down 3,Sound Up 3,Sound Down 3".split(",")
 item_table = "Cicada Shell,Dragonfly Wing,Waterweed,Leaf,White Flower,Orange Flower,Pink Flower,Unknown Fossil A,Rain Crystal,Beautiful Small Stone,Fang,Bird Wing,Emerald,Ruby,Piece of Ruin A,Piece of Ruin B,Diamond,Squid Grass,Tree Honey,Ancient Tools A,Ancient Tools B,Tin Toy,Aquamarine,Scent Bag,Nut,Propeller Grass,Weight,Turquoizo Husk,Crystallized Thunder,Piece of Shooting Star,Stray Shooting Star,Unknown Fossil B,Ancient Water,Shrimp Barbel,Button,Small Fire,Sapphire,Ancient Spoon,Mysterious Lithograph 1,Mysterious Lithograph 2,Mysterious Lithograph 3,Bone A,Bone B,Big Fallen Leaf,Honey,Snail Shell,Pearl,Small Web,Wooden Board,Aromatic Mushroom,Fountainhead Water,Golden Apple,Pebble,Cheese,Scorpion Trail,Green Axe Carapace,Small Mountain,Small Wind,Balloon Flower,Dried Nuts,Tackle Box,Rod,Diary,Diary Page,Recipe".split(",")
 environments = "The Lost Ruins,The Missing Jungle,The Big Tree,The Dish Pond,The Haunted Cave,The Bush River,Map 6,The Pocket Sea,The Final Jungle".split(",")
+lures = "Dragonflew,Froggy,Quest Worm,Pointer,Cicadar,Glass Star,Rain Dolphin,Emerald Leaf,Spinny Fin,Shad Blade,Dig Deep,Frogger,Hopper,Shooting Flower,Bug Worm,Kraken,Femme,Squid Lure,Lily,Old Stuff,Laura,Flame Toy,Ballith,Squiddy,Spinnow,Ballista,Jeweler,Buster,Sharky,Tifoss,Daram,Rabbit,Kurokuro,Verge,Scorpy,Buzz,Death Witch,Buggy".split(",")
+env_count = len(environments)
+lurecount = len(lures)
+for i in range(256-len(environments)):
+    environments.append("Invalid (ID: " + str(i+env_count) + ")")
+for i in range(256-len(lures)):
+    lures.append("Invalid (ID: " + str(i+lurecount) + ")")
 
 def process_file(filename, mode, slot_sel, output):
     with open(filename, 'rb') as f:
@@ -10,48 +17,51 @@ def process_file(filename, mode, slot_sel, output):
         empty = False
         if not byte_s:
             return
-        if not byte_s.decode("ascii") == "BuzzRod":
+        try:
+            if not byte_s.decode("ascii") == "BuzzRod":
+                print("Not a BuzzRod game save")
+                return
+            else:
+                f.seek(slot_sel*0x820+8)
+                empty = not int.from_bytes(f.read(1), "little") == 3
+                if empty:
+                    print("This save is empty.")
+                    return
+                if mode == "area":
+                    f.seek(slot_sel*0x820+0xC)
+                    area = int.from_bytes(f.read(1), "little")
+                    print(environments[area] + " (ID: " + str(area) + ", byte: " + hex(slot_sel*0x820+0xC) + ")")
+                    return
+                if mode[:4] == "list":
+                    print("ID".ljust(10), "Name".ljust(30), "Count".ljust(10), "Type".ljust(20), "Byte".ljust(10), "Raw".ljust(5))
+                if mode == "list slots" or mode == "list":
+                    f.seek(0x589+(slot_sel*0x820))
+                    seek = 0x589+(slot_sel*0x820)
+                    for i in range(len(slot_table)):
+                        byte_s = f.read(4)
+                        count = int(int.from_bytes(byte_s, "little") / 2)
+                        consumptive = False
+                        if count >= 32:
+                            count = count - 32
+                            consumptive = True
+                        if count > 0 and not consumptive:
+                            print(str(i).ljust(10), slot_table[i].ljust(30), "".ljust(10), "Slot".ljust(20), hex(seek).ljust(10), int.from_bytes(byte_s, "little"))
+                        if count > 0 and consumptive:
+                            print(str(i).ljust(10), slot_table[i].ljust(30), str(count).ljust(10), "Slot (consumptive)".ljust(20), hex(seek).ljust(10), int.from_bytes(byte_s, "little"))
+                        seek += 4
+                if mode == "list items" or mode == "list":
+                    f.seek(0x589+(slot_sel*0x820)+(4*len(slot_table)))
+                    seek = 0x589+(slot_sel*0x820)+(4*len(slot_table))
+                    for i in range(len(item_table)):
+                        byte_s = f.read(4)
+                        count = int(int.from_bytes(byte_s, "little") / 2)
+                        if count >= 32:
+                            count = count - 32
+                        if count > 0:
+                            print(str(i).ljust(10), item_table[i].ljust(30), str(count).ljust(10), "Item".ljust(20), hex(seek).ljust(10), int.from_bytes(byte_s, "little"))
+                        seek += 4
+        except:
             print("Not a BuzzRod game save")
-            return
-        else:
-            f.seek(slot_sel*0x820+8)
-            empty = not int.from_bytes(f.read(1), "little") == 3
-            if empty:
-                print("This save is empty.")
-                return
-            if mode == "area":
-                f.seek(slot_sel*0x820+0xC)
-                area = int.from_bytes(f.read(1), "little")
-                print(environments[area] + " (ID: " + str(area) + ", byte: " + hex(slot_sel*0x820+0xC) + ")")
-                return
-            if mode[:4] == "list":
-                print("ID".ljust(10), "Name".ljust(30), "Count".ljust(10), "Type".ljust(20), "Byte".ljust(10), "Raw".ljust(5))
-            if mode == "list slots" or mode == "list":
-                f.seek(0x589+(slot_sel*0x820))
-                seek = 0x589+(slot_sel*0x820)
-                for i in range(len(slot_table)):
-                    byte_s = f.read(4)
-                    count = int(int.from_bytes(byte_s, "little") / 2)
-                    consumptive = False
-                    if count >= 32:
-                        count = count - 32
-                        consumptive = True
-                    if count > 0 and not consumptive:
-                        print(str(i).ljust(10), slot_table[i].ljust(30), "".ljust(10), "Slot".ljust(20), hex(seek).ljust(10), int.from_bytes(byte_s, "little"))
-                    if count > 0 and consumptive:
-                        print(str(i).ljust(10), slot_table[i].ljust(30), str(count).ljust(10), "Slot (consumptive)".ljust(20), hex(seek).ljust(10), int.from_bytes(byte_s, "little"))
-                    seek += 4
-            if mode == "list items" or mode == "list":
-                f.seek(0x589+(slot_sel*0x820)+(4*len(slot_table)))
-                seek = 0x589+(slot_sel*0x820)+(4*len(slot_table))
-                for i in range(len(item_table)):
-                    byte_s = f.read(4)
-                    count = int(int.from_bytes(byte_s, "little") / 2)
-                    if count >= 32:
-                        count = count - 32
-                    if count > 0:
-                        print(str(i).ljust(10), item_table[i].ljust(30), str(count).ljust(10), "Item".ljust(20), hex(seek).ljust(10), int.from_bytes(byte_s, "little"))
-                    seek += 4
 
 def modify_byte(f, output, index, value):
     with open(output, 'wb') as of:
@@ -78,7 +88,8 @@ def fix_file(filename):
             out.write(str.encode("BuzzRod"))
             out.write(int(0).to_bytes(1, 'little'))
             inp.seek(8)
-            while 1:
+            size = 8
+            while size < 10408:
                 byte_s = inp.read(1)
                 if not byte_s:
                     break
@@ -87,8 +98,53 @@ def fix_file(filename):
                 else:
                     out.write(byte_s)
                 idx += 1
+                size += 1
     print("Fixed save file written to " + output)
-            
+
+
+def list_saves(f):
+    print("Slot".ljust(10), "Total weight".ljust(20), "Area".ljust(20), "Date/Time".ljust(30), "Lure".ljust(10))
+    for slot_sel in range(5):
+        save_loc = (slot_sel*0x820)
+        f.seek(save_loc+0xF)
+        lure = lures[int.from_bytes(f.read(1), "little")]
+        f.seek(save_loc+0x10)
+        weight = int.from_bytes(f.read(4), "little")
+        f.seek(save_loc+0xC)
+        area = environments[int.from_bytes(f.read(1), "little")]
+        f.seek(save_loc+0x18)
+        date_year = int.from_bytes(f.read(4), "little")
+        f.seek(save_loc+0x1C)
+        daymonth = int.from_bytes(f.read(4), "little")
+        day = int(bin((daymonth >> 4) & 0b1111), 2)
+        month = int(bin((daymonth >> 0) & 0b1111), 2)
+        f.seek(save_loc+0x1d)
+        registers = int.from_bytes(f.read(1), "little")
+        add2min = int(bin((registers >> 0) & 0b1), 2) == 1
+        add1min = int(bin((registers >> 1) & 0b1), 2)  == 1
+        add12hour = int(bin((registers >> 2) & 0b1), 2)  == 1
+        add8hour = int(bin((registers >> 3) & 0b1), 2)  == 1
+        add2hour = int(bin((registers >> 4) & 0b1), 2)  == 1
+        add2hour2 = int(bin((registers >> 5) & 0b1), 2)  == 1
+        add1hour = int(bin((registers >> 6) & 0b1), 2)  == 1
+        secondhalf = int(bin((registers >> 7) & 0b1), 2)  == 1
+        if secondhalf:
+            day = day + 16
+        hour = 0
+        if add12hour: hour += 16
+        if add8hour: hour += 8
+        if add2hour: hour += 4
+        if add2hour2: hour += 2
+        if add1hour: hour += 1
+        f.seek(save_loc+0x1e)
+        minute = ((int.from_bytes(f.read(1), "little") >> 4) & 15) * 4
+        if add2min: minute += 2
+        if add1min: minute += 1
+        full_date = str(date_year).zfill(4) + "/" + str(month).zfill(2) + "/" + str(day).zfill(2)
+        full_time = str(hour).zfill(2) + ":" + str(minute).zfill(2)
+        print(str(slot_sel+1).ljust(10), (str(weight) + " g").ljust(20), area.ljust(20), (full_date + " " + full_time).ljust(30), lure.ljust(20))
+    
+
 def help():
     filename = os.path.basename(__file__)
     print("BuzzRod save file editor 0.2\n")
@@ -106,6 +162,7 @@ def help():
     print("-nc".ljust(45), "Sets slot item to be non-consumptive")
     print("-a".ljust(45), "Shows the current area on selected save slot")
     print("-v [integer]".ljust(45), "Specifies a value to be written to the byte (do not use this for items or slots)")
+    print("-saves".ljust(45), "Displays list of saves")
     print("-h".ljust(45), "Displays help screen")
     print("\nExamples:\n")
     print(filename + " -f BESLES-53236 -li -save2".ljust(65), "Displays items for the second save slot")
@@ -115,6 +172,7 @@ def help():
     print(filename + " -f BESLES-53236 -p 0x589 -nc -o BESLES_53236.modified".ljust(65), "Gives Best push slot item to save 1")
     print(filename + " -f BESLES-53236 -p 0x1f29 -iv 3 -o BESLES_53236.modified".ljust(65), "Set Honey count to 2 on save 4")
     print(filename + " -f BESLES-53236 -p 0x104c -v 6 -o BESLES_53236.modified".ljust(65), "Set current area on save 3 to Map 6 (normally inaccessible area)")
+    print(filename + " -f BESLES-53236 -saves".ljust(65), "Display details of every save for easy identification")
 def main():
     mode = "file"
     filename = ""
@@ -187,6 +245,10 @@ def main():
             slot = 3
         elif arg == "-save5":
             slot = 4
+        elif arg == "-saves":
+            with open(filename, "rb") as f:
+                list_saves(f)
+            return
         elif arg == "-o":
             mode = "output"
         elif arg == "-h":
